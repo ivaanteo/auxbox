9c10c072093b2947107cfffb3ac046a50efc560d
Ivan <ivanteozhiming@gmail.com>
2021-04-16 14:23:10 +0800

    Initial Commit

---
+++ AuxBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuxBox
+//
+//  Created by Ivan Teo on 16/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
